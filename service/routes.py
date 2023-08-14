6c1894bbd12e2781719758801c7f82672c722508
nikofebrianur <nikofebrianur@gmail.com>
2023-08-13 22:35:45 -0400

    fixing linter blank line

--- service/routes.py
+++ service/routes.py
@@ -17,6 +17,7 @@ def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
 
+
 ######################################################################
 # GET INDEX
 ######################################################################
@@ -31,6 +32,7 @@ def index():
         ),
         status.HTTP_200_OK,
     )
+
 
 ######################################################################
 # CREATE A NEW ACCOUNT
@@ -54,6 +56,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -69,6 +72,7 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -83,6 +87,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -101,6 +106,7 @@ def get_accounts(account_id):
 #     account.update()
 #     return account.serialize(), status.HTTP_200_OK.
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -116,11 +122,10 @@ def delete_accounts(account_id):
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
 
+
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
